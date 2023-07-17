30fc3093e06a93eedff3fcb2db2b964a6bac28e6
jfarenq <53334981+jfarenq@users.noreply.github.com>
2023-07-17 11:37:09 +0200

    test

--- VMware/BDFsingleVM/main.tf
+++ VMware/BDFsingleVM/main.tf
@@ -12,12 +12,10 @@ variable "allow_unverified_ssl" {
 # Define the vsphere provider 
 ##############################################################
 provider "vsphere" {
-  allow_unverified_ssl = "${var.allow_unverified_ssl}"
-  version              = "~> 1.3"
+  allow_unverified_ssl = var.allow_unverified_ssl
 }
 
 provider "camc" {
-  version = "~> 0.2"
 }
 
 ##############################################################
